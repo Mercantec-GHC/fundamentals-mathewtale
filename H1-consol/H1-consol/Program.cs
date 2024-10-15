be7d27f0ce4cb261952e33a3fcb85a7e0ba3dc52
Budget knight <mads.bargsteen@live.dk>
2024-10-15 10:27:51 +0200

    Grade Write

--- H1-consol/H1-consol/Program.cs
+++ H1-consol/H1-consol/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name;
+            float grade;
+            name = "sophia :";
+            grade = (93 + 87 + 98 + 95 + 100) / 5f;
+            Console.WriteLine(name + "   " + grade);
+
+            name = "Nicolas :";
+            grade = (80 + 83 + 82 + 88 + 85) / 5f;
+            Console.WriteLine(name + "   " + grade);
+
+            name = "Zahirah :";
+            grade = (84 + 96 + 73 + 79 + 85) / 5f;
+            Console.WriteLine(name + "   " + grade);
+
+            name = "Jeong :";
+            grade = (90 + 92 + 98 + 100 + 97) / 5f;
+            Console.WriteLine(name + "   " + grade);
+
         }
     }
 }
