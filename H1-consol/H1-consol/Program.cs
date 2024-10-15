ca709992c3398e1b494d660400da3680d88b4942
Budget knight <mads.bargsteen@live.dk>
2024-10-15 10:56:47 +0200

    formatering

--- H1-consol/H1-consol/Program.cs
+++ H1-consol/H1-consol/Program.cs
@@ -6,22 +6,22 @@
         {
             string name;
             float grade;
+            Console.WriteLine("student\t\tGrade\n");
             name = "sophia :";
             grade = (93 + 87 + 98 + 95 + 100) / 5f;
-            Console.WriteLine(name + "   " + grade);
+            Console.WriteLine(name + "\t\t" + grade + "\tA");
 
             name = "Nicolas :";
             grade = (80 + 83 + 82 + 88 + 85) / 5f;
-            Console.WriteLine(name + "   " + grade);
+            Console.WriteLine(name + "\t\t" + grade + "\tB");
 
             name = "Zahirah :";
             grade = (84 + 96 + 73 + 79 + 85) / 5f;
-            Console.WriteLine(name + "   " + grade);
+            Console.WriteLine(name + "\t\t" + grade + "\tB");
 
             name = "Jeong :";
             grade = (90 + 92 + 98 + 100 + 97) / 5f;
-            Console.WriteLine(name + "   " + grade);
-
+            Console.WriteLine(name + "\t\t\t" + grade + "\tA");
         }
     }
 }
